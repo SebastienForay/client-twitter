d4761bba7fed1ada60959a1f8dbd79339d44fe2c
Sébastien <sebastien.foray@ynov.com>
2016-03-07 11:03:41 +0100

    Projet recréé

---
+++ DotNet/TwitterDotNet/TwitterDotNet/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+using System;
+using TwitterDotNet.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
+
+namespace TwitterDotNet.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+        }
+    }
+}
