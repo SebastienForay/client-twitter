be388f5a8975f5fcd0a235363da72819856ef069
Sébastien <sebastien.foray@ynov.com>
2016-03-07 10:21:17 +0100

    Commit Initial DotNet

---
+++ DotNet/ClientTwitter/ClientTwitter/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using ClientTwitter.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace ClientTwitter.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
