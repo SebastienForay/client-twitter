9f2a8227e3d31a571f4bfa550a5f4e1a1beb6461
rafikt <rafik.tighiouaret@ynov.com>
2016-03-11 11:56:49 +0100

    Ajout Swift au repo

---
+++ Swift/TwitterYnov/TwitterYnov/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  TwitterYnov
+//
+//  Created by Supinfo on 10/03/16.
+//  Copyright © 2016 Ynov. All rights reserved.
+//
+
+import UIKit
+import TwitterKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let logInButton = TWTRLogInButton { (session, error) in
+            if let unwrappedSession = session {
+                let alert = UIAlertController(title: "Logged In",
+                    message: "User \(unwrappedSession.userName) has logged in",
+                    preferredStyle: UIAlertControllerStyle.Alert
+                )
+                alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+                self.presentViewController(alert, animated: true, completion: nil)
+            } else {
+                NSLog("Login error: %@", error!.localizedDescription);
+            }
+        }
+        
+        // TODO: Change where the log in button is positioned in your view
+        logInButton.center = self.view.center
+        self.view.addSubview(logInButton)
+
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
