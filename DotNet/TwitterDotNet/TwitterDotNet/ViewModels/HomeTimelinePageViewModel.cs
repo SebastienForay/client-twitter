5aae18b15d0cfb8289daecc42472f9591a273b2f
Sébastien <sebastien.foray@ynov.com>
2016-03-14 10:57:03 +0100

    Modification des Collections de Tweets +Retweet
    Changement en ObservableCollection<Tweet> pour une meilleure gestion.
    Implémentation du Retweet avec des CommandParameter sur les Button du
    xaml.

--- DotNet/TwitterDotNet/TwitterDotNet/ViewModels/HomeTimelinePageViewModel.cs
+++ DotNet/TwitterDotNet/TwitterDotNet/ViewModels/HomeTimelinePageViewModel.cs
@@ -6,7 +6,6 @@ using Template10.Services.NavigationService;
 using Windows.UI.Xaml.Navigation;
 using GalaSoft.MvvmLight.Command;
 using TwitterDotNet.Services.TweetinviAPI;
-using Tweetinvi;
 using Tweetinvi.Core.Credentials;
 using System;
 using Windows.UI.Xaml.Controls;
@@ -14,15 +13,32 @@ using Windows.UI.Xaml;
 using TwitterDotNet.Services.AccountManager;
 using Tweetinvi.Core.Parameters;
 using Tweetinvi.Core.Interfaces;
+using Tweetinvi.Logic;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace TwitterDotNet.ViewModels
 {
     public class HomeTimelinePageViewModel : ViewModelBase
     {
+        public HomeTimelinePageViewModel()
+        {
+            GotoHomeTimelinePageCommand = new RelayCommand(GotoHomeTimeline);
+            GotoNotificationsCommand = new RelayCommand(GotoNotifications);
+            GotoMessagesCommand = new RelayCommand(GotoMessages);
+            GotoFindPeopleCommand = new RelayCommand(GotoFindPeople);
+            GotoSearchCommand = new RelayCommand(GotoSearch);
+
+            GotoProfilPageCommand = new RelayCommand(GotoProfilPage);
+
+            GotoTweetingPageCommand = new RelayCommand(GotoTweetingPage);
+
+            RetweetCommand = new RelayCommand<object>(param => Retweet((string)param));
+        }
+
         public override async Task OnNavigatedToAsync(object parameter, NavigationMode mode, IDictionary<string, object> state)
         {
-            if (Tweets == null)
+            if (TweetsCollection.Count == 0)
                 FirstTimelineLoading();
             else
                 TimelineReloading();
@@ -32,77 +48,87 @@ namespace TwitterDotNet.ViewModels
 
         private void FirstTimelineLoading()
         {
-            HomeTlParameters.MaximumNumberOfTweetsToRetrieve = 10;
-            Tweets = Timeline.GetHomeTimeline(HomeTlParameters);
+            HomeTlParameters.MaximumNumberOfTweetsToRetrieve = 50;
+            var newTweets = Tweetinvi.Timeline.GetHomeTimeline(HomeTlParameters);
+            foreach (var tweet in newTweets)
+            {
+                var curTweet = tweet as Tweet;
+                TweetsCollection.Add(curTweet);
+            }
         }
 
         private void TimelineReloading()
         {
-            HomeTlParameters.MaximumNumberOfTweetsToRetrieve = 10;
-            HomeTlParameters.SinceId = Tweets.ElementAt(0).Id;
+            HomeTlParameters.MaximumNumberOfTweetsToRetrieve = 50;
+            HomeTlParameters.SinceId = TweetsCollection.ElementAt(0).Id;
 
-            var newTweets = Timeline.GetHomeTimeline(HomeTlParameters);
+            var newTweets = Tweetinvi.Timeline.GetHomeTimeline(HomeTlParameters);
             foreach (var tweet in newTweets)
             {
-                TweetsCollection.Insert(0, tweet);
+                var curTweet = tweet as Tweet;
+                TweetsCollection.Insert(0, curTweet);
             }
         }
-
-        public void GotoSettings() => NavigationService.Navigate(typeof(Views.SettingsPage), 0);
-        public void GotoPrivacy() => NavigationService.Navigate(typeof(Views.SettingsPage), 1);
-        public void GotoAbout() => NavigationService.Navigate(typeof(Views.SettingsPage), 2);
-        private void GotoProfilPage() => NavigationService.Navigate(typeof(Views.UserProfilPage));
-        private void GotoHomeTimeline() => TimelineReloading();
 
         private HomeTimelineParameters _homeTlParameters = new HomeTimelineParameters();
         public HomeTimelineParameters HomeTlParameters { get { return _homeTlParameters; } set { _homeTlParameters = value; } }
         
-        private IEnumerable<ITweet> _tweets;
-        public IEnumerable<ITweet> Tweets
+        private static ObservableCollection<Tweet> _tweetsCollection = new ObservableCollection<Tweet>();
+        public ObservableCollection<Tweet> TweetsCollection { get { return _tweetsCollection; } set { _tweetsCollection = value; RaisePropertyChanged(); } }
+
+        // Tweets Commands
+        private RelayCommand<object> _retweetCommand;
+        public RelayCommand<object> RetweetCommand { get { return _retweetCommand; } set { _retweetCommand = value; } }
+
+        private void Retweet(object tweetIdStr)
         {
-            get { return _tweets; }
-            set
-            {
-                _tweets = value;
-                foreach (var tweet in Tweets)
-                {
-                    TweetsCollection.Add(tweet);
-                }
-            }
+            var tweetId = Convert.ToInt64(tweetIdStr);
+            Tweetinvi.Tweet.PublishRetweet((long)tweetId);
         }
 
-
-        private ObservableCollection<ITweet> _tweetsCollection;
-        public ObservableCollection<ITweet> TweetsCollection
-        {
-            get { return _tweetsCollection; }
-            set { _tweetsCollection = value; RaisePropertyChanged(); }
-        }
-
-
-        private RelayCommand _gotoProfilPageCommand;
-        public RelayCommand GotoProfilPageCommand
-        {
-            get
-            {
-                if (_gotoProfilPageCommand == null)
-                    _gotoProfilPageCommand = new RelayCommand(GotoProfilPage);
-
-                return _gotoProfilPageCommand;
-            }
-            set { _gotoProfilPageCommand = value; RaisePropertyChanged(); }
-        }
+        // TopBar Primary Commands
         private RelayCommand _gotoHomeTimelinePageCommand;
-        public RelayCommand GotoHomeTimelinePageCommand
-        {
-            get
-            {
-                if (_gotoHomeTimelinePageCommand == null)
-                    _gotoHomeTimelinePageCommand = new RelayCommand(GotoHomeTimeline);
+        private RelayCommand _gotoNotificationsCommand;
+        private RelayCommand _gotoMessagesCommand;
+        private RelayCommand _gotoFindPeopleCommand;
+        private RelayCommand _gotoSearchCommand;
 
-                return _gotoHomeTimelinePageCommand;
-            }
-            set { _gotoHomeTimelinePageCommand = value; RaisePropertyChanged(); }
+        public RelayCommand GotoHomeTimelinePageCommand { get { return _gotoHomeTimelinePageCommand; } set { _gotoHomeTimelinePageCommand = value; } }
+        public RelayCommand GotoNotificationsCommand { get { return _gotoNotificationsCommand; } set { _gotoNotificationsCommand = value; } }
+        public RelayCommand GotoMessagesCommand { get { return _gotoMessagesCommand; } set { _gotoMessagesCommand = value; } }
+        public RelayCommand GotoFindPeopleCommand { get { return _gotoFindPeopleCommand; } set { _gotoFindPeopleCommand = value; } }
+        public RelayCommand GotoSearchCommand { get { return _gotoSearchCommand; } set { _gotoSearchCommand = value; } }
+
+        // TopBar Secondary Commands
+        private RelayCommand _gotoProfilPageCommand;
+        public RelayCommand GotoProfilPageCommand { get { return _gotoProfilPageCommand; } set { _gotoProfilPageCommand = value; } }
+
+
+        // Commands Functions
+            // Primary
+        private void GotoHomeTimeline()
+        {
+            if (NavigationService.CurrentPageType == typeof(Views.HomeTimelinePage))
+                TimelineReloading();
+            else
+                NavigationService.Navigate(typeof(Views.HomeTimelinePage));
         }
+        private void GotoNotifications() => NavigationService.Navigate(typeof(Views.HomeTimelinePage));
+        private void GotoMessages() => NavigationService.Navigate(typeof(Views.HomeTimelinePage));
+        private void GotoFindPeople() => NavigationService.Navigate(typeof(Views.HomeTimelinePage));
+        private void GotoSearch() => NavigationService.Navigate(typeof(Views.HomeTimelinePage));
+
+            // Secondary
+        private void GotoProfilPage() => NavigationService.Navigate(typeof(Views.UserProfilPage));
+        public void GotoSettings() => NavigationService.Navigate(typeof(Views.SettingsPage), 0);
+        public void GotoPrivacy() => NavigationService.Navigate(typeof(Views.SettingsPage), 1);
+        public void GotoAbout() => NavigationService.Navigate(typeof(Views.SettingsPage), 2);
+
+        // Bottom Bar Commands
+        private RelayCommand _gotoTweetingPageCommand;
+        public RelayCommand GotoTweetingPageCommand { get { return _gotoTweetingPageCommand; } set { _gotoTweetingPageCommand = value; } }
+
+        // Bottom Bar Functions
+        private void GotoTweetingPage() => NavigationService.Navigate(typeof(Views.TweetingPage));
     }
 }
